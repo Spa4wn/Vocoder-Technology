669962a7aa54c01411f72b9f48db55ba12ce8065
Constantino <leitedanadi@gmail.com>
2024-10-05 01:13:12 -0300

    Update script.js

--- Projeto WEB_JS2/script/script.js
+++ Projeto WEB_JS2/script/script.js
@@ -1,4 +1,4 @@
-let usuarios = JSON.parse(localStorage.getItem('usuarios')) || [
+let usuarios = JSON.parse(localStorage.getItem('usuarios')) || [ 
     { username: 'usuario1', password: 'senha1' },
     { username: 'usuario2', password: 'senha2' }
 ];
@@ -12,15 +12,18 @@ function register(event) {
     const newUsername = document.getElementById('newUsername').value;
     const newPassword = document.getElementById('newPassword').value;
 
-    // Verifica se os campos estão preenchidos e têm pelo menos 3 caracteres
-    if (!newUsername || !newPassword || newUsername.length < 3 || newPassword.length < 3) {
-        showMessage('Por favor, preencha todos os campos com pelo menos 3 caracteres.', 'registerMessage', 'red');
+    if (!newUsername || !newPassword) {
+        const registerMessage = document.getElementById('registerMessage');
+        registerMessage.textContent = 'Por favor, preencha todos os campos.';
+        registerMessage.style.color = 'red';
         return;
     }
 
     const userExists = usuarios.some(user => user.username === newUsername);
     if (userExists) {
-        showMessage('Este nome de usuário já está em uso.', 'registerMessage', 'red');
+        const registerMessage = document.getElementById('registerMessage');
+        registerMessage.textContent = 'Este nome de usuário já está em uso.';
+        registerMessage.style.color = 'red';
         return;
     }
 
@@ -29,15 +32,6 @@ function register(event) {
 
     alert('Cadastro realizado com sucesso! Você pode fazer login agora.');
     window.location.href = 'index.html';
-}
-
-function showMessage(message, elementId, color) {
-    const messageElement = document.getElementById(elementId);
-    messageElement.textContent = message;
-    messageElement.style.color = color;
-    setTimeout(() => {
-        messageElement.textContent = '';
-    }, 3000); // Mensagem desaparece após 3 segundos
 }
 
 function login(event) {
@@ -51,7 +45,9 @@ function login(event) {
         localStorage.setItem('loggedIn', 'true');
         window.location.href = 'index2.html';
     } else {
-        showMessage('Usuário ou senha inválidos.', 'loginMessage', 'red');
+        const loginMessage = document.getElementById('loginMessage');
+        loginMessage.textContent = 'Usuário ou senha inválidos.';
+        loginMessage.style.color = 'red';
     }
 }
 
@@ -77,7 +73,7 @@ function addToCart(name, price) {
         cart.push({ name: name, price: price, quantity: 1 });
     }
 
-    localStorage.setItem('cart', JSON.stringify(cart));
+    localStorage.setItem('cart', JSON.stringify(cart));  
     alert(`${name} foi adicionado ao carrinho.`);
 }
 
@@ -95,11 +91,10 @@ function updateCart() {
 
     let total = 0;
     cart.forEach(item => {
-        let itemTotalPrice = item.price * item.quantity;
-
-        // Aplica desconto se a quantidade for 3 ou mais
+        let itemPrice = item.price;
+        
         if (item.quantity >= 3) {
-            itemTotalPrice *= 0.5; // Aplica 50% de desconto
+            itemPrice = item.price * 0.5;  
         }
 
         const itemElement = document.createElement("div");
@@ -110,7 +105,7 @@ function updateCart() {
             <button onclick="removeFromCart('${item.name}')">Remover</button>
         `;
         cartElement.appendChild(itemElement);
-        total += itemTotalPrice;
+        total += itemPrice * item.quantity;
     });
 
     cartTotalElement.innerHTML = `Total: R$ ${total.toFixed(2)}`;
@@ -121,6 +116,22 @@ function removeFromCart(name) {
     cart = cart.filter(item => item.name !== name);
     localStorage.setItem('cart', JSON.stringify(cart));
     updateCart();
+}
+
+function calcularPromocao(preco, nomeProduto) {
+    const precoPromocional = preco * 0.5; 
+    const mensagem = `Leve 3 unidades de ${nomeProduto} por R$ ${precoPromocional.toFixed(2)} cada!`;
+
+    const idProdutoFormatado = `promocao-${nomeProduto.normalize("NFD").replace(/[\u0300-\u036f]/g, "").replace(/\s+/g, '-').replace(/--+/g, '-').replace(/[^\w-]/g, '')}`;
+    
+    console.log(`ID gerado: ${idProdutoFormatado}`);  
+
+    const promoElement = document.getElementById(idProdutoFormatado);
+    if (promoElement) {
+        promoElement.textContent = mensagem;
+    } else {
+        console.error(`Elemento ${idProdutoFormatado} não encontrado.`);
+    }
 }
 
 function finalizarCompra() {
@@ -137,7 +148,7 @@ function finalizarCompra() {
     }
 
     alert(`Compra finalizada com sucesso! Método de pagamento: ${paymentMethod.value}`);
-
+    
     localStorage.removeItem('cart');
     window.location.href = 'index2.html';
 }
@@ -179,79 +190,8 @@ document.addEventListener('DOMContentLoaded', () => {
                 const precoText = produtoElement.querySelector('p').textContent;
                 const preco = parseFloat(precoText.replace('Preço: R$', '').replace(',', '.'));
                 addToCart(nome, preco);
+            
             });
         });
     }
-
-    carregarPerfilDoLocalStorage();
-
-    const editProfileButton = document.getElementById('editProfileButton');
-    const editProfileModal = document.getElementById('editProfileModal');
-    const closeButton = editProfileModal.querySelector('.close-button');
-    const editProfileForm = document.getElementById('editProfileForm');
-
-    editProfileButton.addEventListener('click', () => {
-        editProfileModal.style.display = 'block';
-        preencherFormularioDeEdicao();
-    });
-
-    closeButton.addEventListener('click', () => {
-        editProfileModal.style.display = 'none';
-    });
-
-    window.addEventListener('click', (event) => {
-        if (event.target === editProfileModal) {
-            editProfileModal.style.display = 'none';
-        }
-    });
-
-    editProfileForm.addEventListener('submit', (event) => {
-        event.preventDefault();
-        const username = document.getElementById('editUsername').value;
-        const email = document.getElementById('editEmail').value;
-        const bio = document.getElementById('editBio').value;
-        const profilePicture = document.getElementById('editProfilePicture').files[0];
-
-        let profilePictureUrl = 'default-profile.png';
-
-        if (profilePicture) {
-            const reader = new FileReader();
-            reader.onload = function(e) {
-                profilePictureUrl = e.target.result; 
-                salvarPerfilNoLocalStorage(username, email, bio, profilePictureUrl);
-                carregarPerfilDoLocalStorage();
-                editProfileModal.style.display = 'none';
-            };
-            reader.readAsDataURL(profilePicture);
-        } else {
-            salvarPerfilNoLocalStorage(username, email, bio, profilePictureUrl);
-            carregarPerfilDoLocalStorage();
-            editProfileModal.style.display = 'none';
-        }
-    });
 });
-
-function carregarPerfilDoLocalStorage() {
-    const perfil = JSON.parse(localStorage.getItem('perfil'));
-    if (perfil) {
-        document.getElementById('perfilUsername').textContent = perfil.username;
-        document.getElementById('perfilEmail').textContent = perfil.email;
-        document.getElementById('perfilBio').textContent = perfil.bio;
-        const profileImage = document.getElementById('profileImage');
-        profileImage.src = perfil.profilePicture || 'default-profile.png';
-    }
-}
-
-function preencherFormularioDeEdicao() {
-    const perfil = JSON.parse(localStorage.getItem('perfil'));
-    if (perfil) {
-        document.getElementById('editUsername').value = perfil.username;
-        document.getElementById('editEmail').value = perfil.email;
-        document.getElementById('editBio').value = perfil.bio;
-    }
-}
-
-function salvarPerfilNoLocalStorage(username, email, bio, profilePicture) {
-    const perfil = { username, email, bio, profilePicture };
-    localStorage.setItem('perfil', JSON.stringify(perfil));
-}
