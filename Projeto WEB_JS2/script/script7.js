ce414cb1f1cfc815d10337116d9f1ed9caa05568
Constantino <leitedanadi@gmail.com>
2024-10-05 13:08:39 -0300

    Update script7.js

--- Projeto WEB_JS2/script/script7.js
+++ Projeto WEB_JS2/script/script7.js
@@ -1,34 +1,25 @@
-// script7.js
-
 document.addEventListener("DOMContentLoaded", function() {
     const editProfileButton = document.getElementById("editProfileButton");
     const editProfileModal = document.getElementById("editProfileModal");
     const closeButton = document.querySelector(".close-button");
     const editProfileForm = document.getElementById("editProfileForm");
 
-    
     function openModal() {
         editProfileModal.style.display = "block";
     }
 
-    
     function closeModal() {
         editProfileModal.style.display = "none";
     }
 
-    
     editProfileButton.addEventListener("click", openModal);
-
-    
     closeButton.addEventListener("click", closeModal);
 
-    
     window.addEventListener("click", function(event) {
         if (event.target === editProfileModal) {
             closeModal();
         }
     });
-
 
     editProfileForm.addEventListener("submit", function(event) {
         event.preventDefault();
@@ -38,12 +29,10 @@ document.addEventListener("DOMContentLoaded", function() {
         const bio = document.getElementById("editBio").value;
         const profilePictureInput = document.getElementById("editProfilePicture");
 
-      
         document.getElementById("usernameDisplay").textContent = username;
         document.getElementById("emailDisplay").textContent = email;
         document.getElementById("bioDisplay").textContent = bio;
 
-        
         if (profilePictureInput.files.length > 0) {
             const reader = new FileReader();
             reader.onload = function(e) {
@@ -52,14 +41,12 @@ document.addEventListener("DOMContentLoaded", function() {
             reader.readAsDataURL(profilePictureInput.files[0]);
         }
 
-        
         closeModal();
     });
 
-    
     const logoutButton = document.getElementById("logoutButton");
     logoutButton.addEventListener("click", function() {
         alert("Você saiu da sua conta.");
-        
+        window.location.href = "index.html";
     });
 });
