520662b282f4a126af26c5ce75fdfd698f071f62
Constantino <leitedanadi@gmail.com>
2024-10-11 23:07:10 -0300

    Update script.js

--- Projeto WEB_JS2/script/script.js
+++ Projeto WEB_JS2/script/script.js
@@ -5,12 +5,10 @@ let usuarios = JSON.parse(localStorage.getItem('usuarios')) || [
 
 function cep() {
     let cep = document.queryselector(".cep").value
-    console.log(cep)
     dados(cep)
 }
 async function dados(cep) {
     let dados = await fetch(`https://viacep.com.br/ws/${cep}/json/`).then(response => response.json())
-    console.log(dados)
     permuta(dados)
 }
 function permuta(dados) {
